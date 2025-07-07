5cadbc46c93c2c7c476e2de974f9bcd070a95bdd
sergii.ignatov <s.ignatov92@gmail.com>
2025-07-07 22:32:14 +0300

    typo

--- js/todolist.js
+++ js/todolist.js
@@ -15,7 +15,7 @@ export default class ToDoList {
     removeItemFromList(id) {
         const list = this._list;
         for (let i = 0; i < list.length; i++) {
-            if (list[i]._id === id) {
+            if (list[i]._id == id) {
                 list.splice(i, 1);
                 break;
             }
